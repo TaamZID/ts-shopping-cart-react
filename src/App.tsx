661c96103e499e41e18e007f9e5a977d4997b44f
TaamZID <tamjidalam00@gmail.com>
2023-01-17 01:54:58 +0600

    typescript shopping cart

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { Container } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+
+function App() {
+  return (
+    <ShoppingCartProvider>
+      <Navbar></Navbar>
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/store" element={<Store></Store>}></Route>
+          <Route path="/About" element={<About></About>}></Route>
+        </Routes>
+      </Container>
+    </ShoppingCartProvider>
+  );
+}
+
+export default App;
